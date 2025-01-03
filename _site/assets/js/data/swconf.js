80de3893ce196abb7291e8dc0a6514db6e93901f
imcpap <syutongwang@hotmail.com>
2025-01-03 09:08:37 +0800

    Change the information

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735865737',resources: [
+    cacheName: 'chirpy-1735866152',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
